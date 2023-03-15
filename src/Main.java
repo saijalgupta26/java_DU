28fa89296d2710c27ded3a6e4c826ea4d77528e0
saijalgupta26 <126044522+saijalgupta26@users.noreply.github.com>
2023-03-15 20:40:48 +0530

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,20 @@
+public class Main{
+    // class name should be upper case
+    public static void main(String[] args) {
+        //package name should be lower case lika mybookshop
+        int num1=10;//var name should be lower case
+        int num2=20;
+        System.out.println(num1+num2);
+        System.out.println(sum(num1,num2));
 
+    }
+    public static int sum(int num1,int num2)// method name should be lower case
+    {
+        return num1+num2;
+    }
+    //constant name should be all uppercase
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 //enum Level {
 //    LOW,
 //    MEDIUM,
